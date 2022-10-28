279d76ef0e4e37e1702384be46adf66c971a41e9
Tom Swain <113206264+4EightyDev@users.noreply.github.com>
2022-10-28 15:15:02 -0700

    edits

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -10,17 +10,17 @@ const OurTeam = () => {
                     <div className="container-2xl md:flex">
                          <div className='md:w-1/2 lg:w-5/12 xl:w-1/3 pr-4 pl-2'>
                               <div className='sticky top-[185px]'>
-                                   <CardTeamMember name='Joe Swain' img={TeamJoe} position='Owner / Founder' bio='With more than forty years of woodworking experience and a passion for quality, Joe Swain has spent a lifetime developing his skills in the woodworking industry.' />
+                                   <CardTeamMember name='Joe Swain' img={TeamJoe} position='Owner / Founder' bio='With more than forty years of woodworking experience and a passion for quality, Joe has spent a lifetime developing his skills in the woodworking industry.' />
                               </div>
                          </div>
                          <div className='md:w-1/2 lg:w-7/12 xl:w-2/3 grid md:grid-cols-1 lg:grid-cols-2'>
                               <CardTeamMember name='Christine Siller' img={TeamChristine} position='Business Manager' />
-                              <CardTeamMember name='Mariah Gallegos' img={TeamMariah} position='Design Specialist / Product Manager' />
+                              <CardTeamMember name='Mariah Gallegos' img={TeamMariah} position='Design Specialist / Project Manager' />
+                              <CardTeamMember name='Paull Wulffson' img={TeamPaull} position='Shop Manager' />
+                              <CardTeamMember name='Nic Serra' img={TeamNick} position='Installation Manager' />
                               <CardTeamMember name="Will O'Fallon" img={TeamWill} position='Design Specialist' />
                               <CardTeamMember name='Scott Vance' img={TeamScott} position='Senior Cabinetmaker' />
-                              <CardTeamMember name='Paull Wulffson' img={TeamPaull} position='Senior Cabinetmaker' />
                               <CardTeamMember name='Brandon Brian' img={TeamBrandon} position='Finisher' />
-                              <CardTeamMember name='Nic Serra' img={TeamNick} position='Installation Manager' />
                               <CardTeamMember name='Jose Olivarria' img={TeamJose} position='Finisher' />
                               <CardTeamMember name='Dave West' img={TeamDave} position='Installer' />
                               <CardTeamMember name='Thomas Patch' img={TeamThomas} position='Assembler' />
