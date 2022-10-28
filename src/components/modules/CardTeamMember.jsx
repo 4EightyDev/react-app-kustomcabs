b4328e2b0cd27fe2ce1b48791717780d1eaa4377
Tom Swain <113206264+4EightyDev@users.noreply.github.com>
2022-10-27 21:52:02 -0700

    fixed z-index issue on page title

--- src/components/modules/CardTeamMember.jsx
+++ src/components/modules/CardTeamMember.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CardTeamMember = (props) => {
      return (
           <figure class="team-member flex-center flex-col text-center p-2 max-w-md mx-auto">
-               <img src={props.img} alt={props.name} className='bg-white border-8 border-white -mb-[60px] max-w-[90%] z-30' />
+               <img src={props.img} alt={props.name} className='bg-white border-8 border-white -mb-[60px] max-w-[90%] z-10' />
                <figcaption class="member-info p-4 pt-[60px] w-full bg-white border">
                     <h4 class="font-serif text-2xl">{props.name}</h4>
                     <p class="text-[11px] text-gold uppercase tracking-wide">{props.position}</p>
