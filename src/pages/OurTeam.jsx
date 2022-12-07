065973044473aa74bf6ae73448e6d861f878017c
Tom Swain <113206264+4EightyDev@users.noreply.github.com>
2022-12-07 11:51:59 -0700

    removed team member: jose

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -21,7 +21,6 @@ const OurTeam = () => {
                               <CardTeamMember name="Will O'Fallon" img={TeamWill} position='Design Specialist' />
                               <CardTeamMember name='Scott Vance' img={TeamScott} position='Senior Cabinetmaker' />
                               <CardTeamMember name='Brandon Brian' img={TeamBrandon} position='Finish Specialist' />
-                              <CardTeamMember name='Jose Olivarria' img={TeamJose} position='Finish Specialist' />
                               <CardTeamMember name='Dave West' img={TeamDave} position='Installation Specialist' />
                               <CardTeamMember name='Thomas Patch' img={TeamThomas} position='Assembly Specialist' />
                          </div>
